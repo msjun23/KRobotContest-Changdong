68d7d4a75ac59d564719f1dc5e2b90763a8c554e
msjun23 <msjun23@gmail.com>
2021-11-18 10:45:13 +0900

    Update stopbar contents

--- Stopbar_blockcoding_/Stopbar_blockcoding_.ino
+++ Stopbar_blockcoding_/Stopbar_blockcoding_.ino
@@ -1,8 +1,8 @@
 #include <Servo.h> 
-#include <SimpleTimer.h>
 
 #define SIGNAL_1 5
 #define servoPin 9
+#define LED_PIN 13
 
 #define STOPANGLE 105
 #define GOANGLE   20
@@ -11,14 +11,16 @@ Servo servo;
 int angle = 0; // servo position in degrees 
 
 void setup() 
-{ 
+{
+  pinMode(LED_PIN, OUTPUT);
   pinMode(SIGNAL_1, INPUT);
   servo.attach(servoPin); 
-} 
+}
 
 void loop() 
 {
   if (digitalRead(SIGNAL_1) == LOW){
+    digitalWrite(LED_PIN, HIGH);
     delay(10 * 1000);
     
     servo.write(GOANGLE);
@@ -28,5 +30,6 @@ void loop()
   }
 
   servo.write(STOPANGLE);
+  digitalWrite(LED_PIN, LOW);
   delay(15);
 }
